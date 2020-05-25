fe1055ea6b3cf6f75df2e3f4a2fe9d44c849e3cc
Durgasrikanth2020 <srikanth.anaparthi@gmail.com>
2020-05-25 18:44:45 +0530

    Update firstone.js

--- firstone.js
+++ firstone.js
@@ -1,2 +1,15 @@
-let hello = 'Hello world from node js';
-console.log(hello+__dirname);
+
+import express from 'express';
+
+
+//const express = require('express');
+const app = express();
+const PORT = 4000;
+
+app.get('/',(req,res) => {
+    res.send(`node and express server is running on port ${PORT}`);
+});
+
+app.listen(PORT, () => {
+    console.log("server is running");
+})
